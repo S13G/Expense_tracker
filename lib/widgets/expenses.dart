b6d97be81b811bbf0ab297fb5d9060ee19f2b466
s13g <ayflix0@gmail.com>
2023-10-16 02:02:57 +0100

    done with app and also worked on dark mode

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -92,9 +93,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text(
-            'The chart',
-          ),
+          Chart(expenses: _registeredExpenses),
           // wrap in expanded because you're having a column in a column
           Expanded(
             child: mainContent,
